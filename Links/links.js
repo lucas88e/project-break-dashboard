e9c57a0654efc14b531c07c54a9ed95bb6c9fc6a
LUCAS <0910lmadrid4f@gmail.com>
2024-04-08 22:37:13 +0200

    link

--- Links/links.js
+++ Links/links.js
@@ -15,10 +15,15 @@ function mostrarEnlace(enlace){
     containerEnlace.innerHTML += `<li><a id="links" href="${enlace.link}" target="blank">${enlace.nombres}</a><button class="btnRemove"> X </button>`
     const btnRemove = document.querySelectorAll(".btnRemove")
     btnRemove.forEach(button =>{
-    button.addEventListener("click",()=>{ console.log(nombres) 
-    localStorage.removeItem("enlaces");
-
-containerEnlace.innerHTML=""})}
+    button.addEventListener("click",()=>{ 
+        const index =[...button.parentNode.children].indexOf(button.parentNode);
+        const enlaces=JSON.parse(localStorage.getItem("enlaces"))||[]
+            enlaces.splice(index,1)
+            localStorage.setItem("enlaces",JSON.stringify(enlaces))
+            button.parentNode.remove()
+        }
+       
+    )}
     )}
 
 function generarLink() {
@@ -26,7 +31,7 @@ function generarLink() {
         let nombres = nombreLink.value
 
         let link = enlace.value
-        if (nombres ===""){alert("Debe introducir un nombre")}
+        if (nombres ==="" || link===""){alert("Debe introducir un nombre")}
         else{
         let enlacesGuardado =JSON.parse(localStorage.getItem("enlaces"))||[]
         enlacesGuardado.push({nombres, link})
@@ -43,12 +48,15 @@ function generarLink() {
 
 generarLink()
 
-window.addEventListener("load",()=>{
-    let enlacesGuardado=JSON.parse(localStorage.getItem("enlaces"));
-    enlacesGuardado.forEach(enlace => mostrarEnlace(enlace))
-        
-    });
 
+window.addEventListener("load", () => {
+    let enlacesGuardado = localStorage.getItem("enlaces");
+    if (enlacesGuardado) {
+        enlacesGuardado = JSON.parse(enlacesGuardado);
+        enlacesGuardado.forEach(enlace => mostrarEnlace(enlace))
+    }
+
+});
 
 
 
