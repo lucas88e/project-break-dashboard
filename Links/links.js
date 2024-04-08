1cdb9390912a458c0aefe403a4c80b745bfcd88a
LUCAS <0910lmadrid4f@gmail.com>
2024-04-08 21:06:58 +0200

    contraseña

--- Links/links.js
+++ Links/links.js
@@ -9,11 +9,13 @@ import { randomImg } from "../reloj/function.js";
 
 randomImg()
 function mostrarEnlace(enlace){
+    let nombres = nombreLink.value.trim
+
     
     containerEnlace.innerHTML += `<li><a id="links" href="${enlace.link}" target="blank">${enlace.nombres}</a><button class="btnRemove"> X </button>`
     const btnRemove = document.querySelectorAll(".btnRemove")
     btnRemove.forEach(button =>{
-    button.addEventListener("click",()=>{ console.log("click") 
+    button.addEventListener("click",()=>{ console.log(nombres) 
     localStorage.removeItem("enlaces");
 
 containerEnlace.innerHTML=""})}
@@ -24,16 +26,16 @@ function generarLink() {
         let nombres = nombreLink.value
 
         let link = enlace.value
-        
+        if (nombres ===""){alert("Debe introducir un nombre")}
+        else{
         let enlacesGuardado =JSON.parse(localStorage.getItem("enlaces"))||[]
         enlacesGuardado.push({nombres, link})
         localStorage.setItem("enlaces", JSON.stringify(enlacesGuardado))
         nombreLink.value="";
         enlace.value="";
         
-        mostrarEnlace({ nombres, link });
+       mostrarEnlace({ nombres, link });}
           
-    console.log(enlacesGuardado)
 
        
     } )
