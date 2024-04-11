63c2627e1ab0adf494505b38df4998e4f212fcb3
LUCAS <0910lmadrid4f@gmail.com>
2024-04-11 21:39:07 +0200

    pruebaPages

--- reloj/function.js
+++ reloj/function.js
@@ -4,17 +4,17 @@ function randomImg() {
   let backgrounds = [];
 
 
-  backgrounds[0] = `../assets/imgs/fondo-1.jpg`
-    backgrounds[1] = `../assets/imgs/fondo-2.jpg`
-  backgrounds[2] = `../assets/imgs/fondo-3.jpg`
-  backgrounds[3] = `../assets/imgs/fondo-4.jpg`
-  backgrounds[4] = `../assets/imgs/fondo-5.jpg`
-  backgrounds[5] = `../assets/imgs/fondo-6.jpg`
-  backgrounds[6] = `../assets/imgs/fondo-7.jpg`
-  backgrounds[7] = `../assets/imgs/fondo-8.jpg`
-  backgrounds[8] = `../assets/imgs/fondo-9.jpg`
-  backgrounds[9] = `../assets/imgs/fondo-10.jpg`
-  backgrounds[10] = `../assets/imgsGIT /fondo-11.jpg`
+  backgrounds[0] = `/assets/imgs/fondo-1.jpg`
+    backgrounds[1] = `/assets/imgs/fondo-2.jpg`
+  backgrounds[2] = `/assets/imgs/fondo-3.jpg`
+  backgrounds[3] = `/assets/imgs/fondo-4.jpg`
+  backgrounds[4] = `/assets/imgs/fondo-5.jpg`
+  backgrounds[5] = `/assets/imgs/fondo-6.jpg`
+  backgrounds[6] = `/assets/imgs/fondo-7.jpg`
+  backgrounds[7] = `/assets/imgs/fondo-8.jpg`
+  backgrounds[8] = `/assets/imgs/fondo-9.jpg`
+  backgrounds[9] = `/assets/imgs/fondo-10.jpg`
+  backgrounds[10] = `/assets/imgs/fondo-11.jpg`
   let randomBackgrounds = Math.floor(Math.random() * backgrounds.length)
 
   body.style.backgroundImage = `url(${backgrounds[randomBackgrounds]})`
