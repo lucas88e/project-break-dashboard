8d1344f3112b0bfd161fe41bbeeb68c5438df9ce
LUCAS <0910lmadrid4f@gmail.com>
2024-04-11 17:48:33 +0200

    gitpages

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const btnContraseña=document.getElementById("btnContraseña")
-const btnTiempo=document.getElementById("btnTiempo")
-const btnLink=document.getElementById("btnLink")
-const btnReloj=document.getElementById("btnReloj")
 
-import { randomImg } from "./reloj/function.js";
+import { randomImg } from "/reloj/function.js";
 randomImg()
  
